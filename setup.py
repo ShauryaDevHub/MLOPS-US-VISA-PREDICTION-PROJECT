fe67c7d363a0fc8667ae474af31a7314f5130582
ShauryaHassHass <sjain42@syr.edu>
2024-11-21 20:31:07 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Raya",
+    author_email="jain19shaurya@gmail",
+    packages=find_packages()
+)
